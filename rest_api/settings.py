7ac7382a05f8f985d48c3e2e7883b3acf1f4d3e2
Wung Brandon <wungbrandon27@gmail.com>
2024-07-21 15:51:18 +0100

    added pagination code and also in the settings

--- rest_api/settings.py
+++ rest_api/settings.py
@@ -54,6 +54,9 @@ REST_FRAMEWORK={
         'rest_framework.authentication.BasicAuthentication',
 
     ],
+    'DEFAULT_PAGINATION_CLASS' : 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE' : 5
+    
 
 
 }
--- todo_api/views.py
+++ todo_api/views.py
@@ -1,20 +1,28 @@
 from django.shortcuts import render
 from rest_framework.generics import CreateAPIView, ListAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .serializers import TodoSerializer
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import Todo
-from django_filters import rest_framework as filters
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
+from .pagination import CustomPageNumberPagination
 
 # Create your views here.
 class TodosAPIView(ListCreateAPIView):
     serializer_class = TodoSerializer
     permission_classes = (IsAuthenticated, )
+    pagination_class = CustomPageNumberPagination
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ["id", "title", "is_complete"]
+    search_fields = ["id", "title", "is_complete"]
+    ordering_fields = ["id", "title", "is_complete"]
     
     def perform_create(self, serializer):
         return serializer.save(owner=self.request.user)
     
     def get_queryset(self):
         return Todo.objects.filter(owner=self.request.user)
+        # return Todo.objects.all()
     
 class TodoDetailAPIView(RetrieveUpdateDestroyAPIView):
     serializer_class = TodoSerializer
